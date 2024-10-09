0281474c0629fc255192ec41024044ee676d7908
Viviane Kopietz <142537081+vivi-kopietz@users.noreply.github.com>
2024-10-09 08:15:10 -0300

    jhhkjjh

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,99 @@
 criaCartão(
     "MATEMATICA",
+    "como calcula a somados termos P.A?",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)criaCartão(
+    "MATEMATICA",
     "como calcula a somados termos P.A"
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A?",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A?",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A?",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A?",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A?",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A?",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A?",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
+    'ultiliza-se a formula: ((a1+an)*r)/2'
+)
+criaCartão(
+    "MATEMATICA",
+    "como calcula a somados termos P.A",
     'ultiliza-se a formula: ((a1+an)*r)/2'
 )
