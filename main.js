1f8caebaf8e8d4f2dd1710245dc139e96cc9175f
Viviane Kopietz <142537081+vivi-kopietz@users.noreply.github.com>
2024-10-16 07:50:40 -0300

    fdfgffg

---
+++ main.js
@@ -0,0 +1,6 @@
+function criaCartão (categoria,pergunta,resposta) {
+let conteiner = document.getElementById('conteiner');
+let cartao = document.createElement('article');
+cartao.className = 'cartao';
+
+})
