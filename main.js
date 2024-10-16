e9ffbf93ccecdbf17df0a09cc2943129faf39d06
Viviane Kopietz <142537081+vivi-kopietz@users.noreply.github.com>
2024-10-16 08:06:24 -0300

    eefdfv

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-function criaCartão (categoria,pergunta,resposta) {
-let conteiner = document.getElementById('conteiner');
-let cartao = document.createElement('article');
-cartao.className = 'cartao';
+function criaCartao (caregoria,pergunta,resposta){
+    let conteiner = document.getElementById('conteiner');
+    let cartao = document.createElement('article');
+    cartao.classList= 'cartao';
+    cartao.innerHTML= 
+    <div class = "conteudo-cartao">
+  <h3>categoria</h3>
 
-})
+<div class="pergunta-cartao">
+  <p>pergunta</p>
+</div>
+<div clas = "resposta-cartao">
+  <p>resposta</p>
+ </div>
+</div>
+
+}
