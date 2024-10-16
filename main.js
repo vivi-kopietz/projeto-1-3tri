c1d8308cc43a1b851b0f0a98ede81a060af20080
Viviane Kopietz <142537081+vivi-kopietz@users.noreply.github.com>
2024-10-16 08:07:37 -0300

    hhju

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function criaCartao (caregoria,pergunta,resposta){
     let conteiner = document.getElementById('conteiner');
-    let cartao = document.createElement('article');
+    let Cartao = document.createElement('article');
     cartao.classList= 'cartao';
     cartao.innerHTML= 
     <div class = "conteudo-cartao">
